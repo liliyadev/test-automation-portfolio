f728d3accabaea8efbb9ce0c04f86e143b2be3e3
Liliya Vildanova <liliya.vild@gmail.com>
2026-07-04 16:59:12 -0400

    Run Chrome in headless mode for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,11 @@ from selenium.webdriver.chrome.options import Options
 def driver(request):
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-save-password-bubble")
 
     chrome_options.add_experimental_option("prefs", {
